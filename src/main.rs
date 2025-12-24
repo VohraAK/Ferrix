29e20e4f58a9216472376bd60b44cbdee3e9e74e
VohraAK <abdullah.k.vohra@gmail.com>
2025-12-25 00:33:26 +0500

    created freestanding Rust binary

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]  // remove std
+#![no_main] // remove main
+
+use core::panic::PanicInfo;
+
+#[panic_handler]    // function called on panic
+fn panic(_info: &PanicInfo) -> !
+{
+    loop{}     
+}
+
+#[unsafe(no_mangle)]    // DO NOT mangle name of this function!
+
+// entry point for the linker (looks for _start)
+pub extern "C" fn _start() -> ! 
+{
+    loop {}
+}
